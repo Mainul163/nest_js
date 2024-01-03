a51017f876cbea27deb41236a3677830aa41c211
unknown <mainulisraf163@gmail.com>
2024-01-03 18:50:54 +0600

    working

--- src/demoCrud.controller.ts
+++ src/demoCrud.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDTO } from './dto';
 
-const USERS = [];
+let USERS = [];
 
 @Controller('/demoCrud')
 export class DemoCrud {
@@ -9,5 +17,29 @@ export class DemoCrud {
   addUser(@Body() createUserDto: CreateUserDTO) {
     USERS.push(createUserDto);
     return 'User added';
+  }
+
+  @Get()
+  getUser() {
+    return USERS;
+  }
+
+  @Get(':id')
+  getUserBYId(@Param('id') id: number) {
+    return USERS.find((user) => user.id === id);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: number, @Body() updateUserDTO: CreateUserDTO) {
+    const userIdx = USERS.findIndex((user) => user.id === id);
+
+    if (!userIdx) {
+      return;
+    }
+    USERS[userIdx] = updateUserDTO;
+  }
+  @Delete('id')
+  deleteUser(@Param('id') id: number) {
+    USERS = USERS.filter((user) => user.id === id);
   }
 }
